2e31e7a494cfaae50c5232a2837356a397e3f4da
7michaelpmcknight <n.velichanskii.924654797@rambler.ru>
2025-12-05 05:42:47 +0800

    add test-adaptive-api.go, ZamaConfig.sol, IKMSVerifier.ts

---
+++ test-adaptive-api.go
@@ -0,0 +1,2 @@
+// Auto-generated file: D:\MyCode\zama\demo\test-adaptive-api.go
+// Generated at: 2025-10-20T08:47:09.679Z
---
+++ utils-factory-inventory/src-static-template.jsx
@@ -0,0 +1,3 @@
+import axios from 'axios';
+
+export const usemain-statistics = () => { return {}; };
